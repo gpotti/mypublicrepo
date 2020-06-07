0a602021dadcc1cd4a15019c3534beb0e07466c0
gpotti <62106613+gpotti@users.noreply.github.com>
2020-06-07 14:08:50 +0530

    iisinstall.ps1
    powershell script

---
+++ iisinstall.ps1
@@ -0,0 +1,3 @@
+#script to install iis on azure scale set 
+Add-WindowsFeature Web-Server
+Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Azure Project Great Learning - host $($env:computername) !"
