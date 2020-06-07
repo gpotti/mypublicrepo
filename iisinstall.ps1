0ed2c9c101ce4fe7c22e4ebe93078ee87988a551
gpotti <62106613+gpotti@users.noreply.github.com>
2020-06-07 15:40:39 +0530

    scriptchange1
    adding shebang

--- iisinstall.ps1
+++ iisinstall.ps1
@@ -1,3 +1,4 @@
+#!/bin/bash
 #script to install iis on azure scale set 
 Add-WindowsFeature Web-Server
 Set-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "Azure Project Great Learning - host $($env:computername) !"
